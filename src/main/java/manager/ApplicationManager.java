b7c21306b7c45e3e4923fdbf49afa43c77fff102
Anastasia Beloded <nastenabeloded@gmail.com>
2022-07-13 21:02:32 +0300

    logger

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
@@ -13,13 +15,17 @@ public class ApplicationManager {
     WebDriver wd;
     HelperUser helperUser;
     HelperCar car;
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
 
     public void init(){
 
         wd = new ChromeDriver();
+        logger.info("All tests run in chrome browser");
         wd.navigate().to("https://ilcarro-1578153671498.web.app/search");
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        logger.info("Current Url---->"+wd.getCurrentUrl());
+
         car = new HelperCar(wd);
 
         helperUser = new HelperUser(wd);
